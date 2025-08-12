c99c9350a5e7a1b8226261e1ffb939df3105100d
Medhiko Biraja <mbirajabiraja@gmail.com>
2025-08-12 10:28:08 +0800

    Initial Commit

---
+++ portos/portosApp.swift
@@ -0,0 +1,17 @@
+//
+//  portosApp.swift
+//  portos
+//
+//  Created by Medhiko Biraja on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct portosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
